d3f356f93ea6aa1e6c0f2c117dc3cc7efac0a0a2
absintminded <absinthminded666@gmail.com>
2020-09-23 11:29:19 +0300

    Implemented task028

--- src/main/java/com/epam/university/java/core/task028/Task028Impl.java
+++ src/main/java/com/epam/university/java/core/task028/Task028Impl.java
@@ -6,24 +6,25 @@ import com.fasterxml.jackson.databind.node.POJONode;
 public class Task028Impl implements Task028 {
     @Override
     public int getWays(int value, int power) {
+
         return count(value, power, 1);
     }
 
     /**
      * Auxiliary function for recursive count.
-     * @param value value.
-     * @param power power.
+     * @param x value.
+     * @param n power.
      * @param num number.
      * @return count.
      */
-    public static int count(int value, int power, int num) {
-        if (Math.pow(num, power) < value) {
-            return count(value, power, ++num)
-                   + count((int)(value - Math.pow(num, power)), power, ++num);
-        } else if (Math.pow(num, power) == value) {
+    public static int count(int x,int n,int num) {
+        if (Math.pow(num, n) < x) {
+            return count(x, n, num + 1) + count((int)(x - Math.pow(num, n)), n, num + 1);
+        } else if (Math.pow(num, n) == x) {
             return 1;
         } else {
             return 0;
         }
     }
+
 }
---
+++ src/main/java/com/epam/university/java/core/task029/Task029Impl.java
@@ -0,0 +1,10 @@
+package com.epam.university.java.core.task029;
+
+import java.util.Collection;
+
+public class Task029Impl implements Task029 {
+    @Override
+    public Collection<String> fillCrossword(Collection<String> rows, Collection<String> words) {
+        return null;
+    }
+}
