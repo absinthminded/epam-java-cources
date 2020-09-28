353adfa5fccb0bb6624e6172cc664668458aac07
absintminded <absinthminded666@gmail.com>
2020-09-28 13:13:22 +0300

    local attempt to 21

---
+++ src/main/java/com/epam/university/java/core/task021/Task021Impl.java
@@ -0,0 +1,12 @@
+package com.epam.university.java.core.task021;
+
+import com.epam.university.java.core.task015.Point;
+
+import java.util.Collection;
+
+public class Task021Impl implements Task021 {
+    @Override
+    public Point calculate(Collection<Point> minePositions) {
+        return null;
+    }
+}
