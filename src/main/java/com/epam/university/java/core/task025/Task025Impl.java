c2c24628ac1faabb5240c2057a5497ea1510b975
absintminded <absinthminded666@gmail.com>
2020-10-06 18:40:24 +0300

    task 025Impl update

--- src/main/java/com/epam/university/java/core/task025/Task025Impl.java
+++ src/main/java/com/epam/university/java/core/task025/Task025Impl.java
@@ -3,25 +3,19 @@ package com.epam.university.java.core.task025;
 public class Task025Impl implements Task025 {
     @Override
     public int getAmountOfAlteredLetters(String sourceMessage) {
-        int amountOfAlteredLetters = 0;
-
         if (sourceMessage == null) {
             throw new IllegalArgumentException();
-        } else if (sourceMessage.length() == 0) {
-            return amountOfAlteredLetters;
         }
 
-        boolean containsFull = sourceMessage.contains("SOS");
-        boolean containsFullAltered = sourceMessage.contains("SAS");
+        int amountOfAlteredLetters = 0;
 
-        if (containsFull || containsFullAltered) {
-            amountOfAlteredLetters = sourceMessage.replaceAll("[SO]", "").length();
-        } else if (!sourceMessage.contains("S")) {
-            amountOfAlteredLetters = sourceMessage.length();
-        } else {
-            amountOfAlteredLetters = sourceMessage.replaceAll("[O]", "").length();
+        String originalSignal = "SOS".repeat(Math.max(0, (sourceMessage.length() + 1) / 2));
+
+        for (int i = 0; i < sourceMessage.length(); i++) {
+            if (sourceMessage.charAt(i) != originalSignal.charAt(i)) {
+                amountOfAlteredLetters++;
+            }
         }
-
 
         return amountOfAlteredLetters;
     }
