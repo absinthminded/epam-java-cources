6bab9ad9d8471103842262cb10c7cad17fa72cf4
absintminded <absinthminded666@gmail.com>
2020-10-15 16:56:18 +0300

    Task048Impl

---
+++ src/main/java/com/epam/university/java/core/task048/Task048Impl.java
@@ -0,0 +1,22 @@
+package com.epam.university.java.core.task048;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.IntStream;
+
+public class Task048Impl implements Task048 {
+    @Override
+    public Collection<Integer> getArmstrongNumbers(Integer from, Integer to) {
+        if (from == null || to == null || from < 0 || to < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return IntStream.range(from + 1, to)
+                .filter((n) -> {
+                    final String number = Integer.toString(n);
+                    return number.chars()
+                            .mapToDouble(v -> Math.pow(v - '0', number.length()))
+                            .sum() == n;
+                }).collect(ArrayList<Integer>::new, ArrayList::add, ArrayList::addAll);
+    }
+}
