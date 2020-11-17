fc9f51361d30797f944d4f0bff5ac6632fbc7f29
absintminded <absinthminded666@gmail.com>
2020-11-17 14:53:33 +0300

    project

--- src/main/java/com/epam/university/java/project/core/cdi/bean/BeanDefinitionReaderImpl.java
+++ src/main/java/com/epam/university/java/project/core/cdi/bean/BeanDefinitionReaderImpl.java
@@ -2,16 +2,51 @@ package com.epam.university.java.project.core.cdi.bean;
 
 import com.epam.university.java.project.core.cdi.io.Resource;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.util.Collection;
 
 public class BeanDefinitionReaderImpl implements BeanDefinitionReader {
+    private final BeanDefinitionRegistry bdRegistry;
+    private final BeanDefinitionToClassRepository bdToClassRepository;
+
+    public BeanDefinitionReaderImpl(BeanDefinitionRegistry registry,
+                                    BeanDefinitionToClassRepository repository) {
+        this.bdRegistry = registry;
+        this.bdToClassRepository = repository;
+    }
+
     @Override
     public int loadBeanDefinitions(Resource resource) {
-        return 0;
+        File resourceFile = resource.getFile();
+        BeanDefinitionsList definitionsList = null;
+        int numberOfDefinitions = 0;
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(BeanDefinitionsList.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            definitionsList = (BeanDefinitionsList) unmarshaller.unmarshal(resourceFile);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        if (definitionsList != null) {
+            for (BeanDefinition definition : definitionsList.getBdList()) {
+                bdRegistry.addBeanDefinition(definition);
+                numberOfDefinitions++;
+            }
+        }
+        return numberOfDefinitions;
     }
 
     @Override
     public int loadBeanDefinitions(Collection<Resource> resources) {
-        return 0;
+        int numberOfDefinitions = 0;
+        for (Resource resource : resources) {
+            numberOfDefinitions += loadBeanDefinitions(resource);
+        }
+        return numberOfDefinitions;
     }
 }
---
+++ src/main/java/com/epam/university/java/project/core/cdi/bean/BeanDefinitionRegistryImpl.java
@@ -0,0 +1,13 @@
+package com.epam.university.java.project.core.cdi.bean;
+
+public class BeanDefinitionRegistryImpl implements BeanDefinitionRegistry {
+    @Override
+    public void addBeanDefinition(BeanDefinition definition) {
+
+    }
+
+    @Override
+    public BeanDefinition getBeanDefinition(String beanId) {
+        return null;
+    }
+}
