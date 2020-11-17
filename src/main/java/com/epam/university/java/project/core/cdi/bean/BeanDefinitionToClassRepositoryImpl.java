3c46c3645128dda41d7a2a9ed94d1f4f14501909
absintminded <absinthminded666@gmail.com>
2020-11-17 14:57:36 +0300

    project

--- src/main/java/com/epam/university/java/project/core/cdi/bean/BeanDefinitionToClassRepositoryImpl.java
+++ src/main/java/com/epam/university/java/project/core/cdi/bean/BeanDefinitionToClassRepositoryImpl.java
@@ -1,13 +1,18 @@
 package com.epam.university.java.project.core.cdi.bean;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BeanDefinitionToClassRepositoryImpl implements BeanDefinitionToClassRepository {
+    private final Map<Class<?>, String> bdToClassRepository = new HashMap<>();
+
     @Override
     public String getBeanId(Class<?> clazz) {
-        return null;
+        return bdToClassRepository.get(clazz);
     }
 
     @Override
     public void putBean(Class<?> clazz, String beanId) {
-
+        bdToClassRepository.put(clazz, beanId);
     }
 }
