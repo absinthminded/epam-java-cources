7b506ed373f37e3154578e034340c2ac9426aa20
absintminded <absinthminded666@gmail.com>
2020-11-19 15:07:33 +0300

    new tests fixes

--- src/main/java/com/epam/university/java/core/task022/Task022Impl.java
+++ src/main/java/com/epam/university/java/core/task022/Task022Impl.java
@@ -1,38 +1,73 @@
 package com.epam.university.java.core.task022;
 
 import java.util.Collection;
-import java.util.Collections;
 
 
 public class Task022Impl implements Task022 {
     @Override
     public int maxSum(Collection<Integer> numbers) {
         validateParams(numbers);
-        int minVal = Collections.min(numbers);
-        int maxSum = 0;
 
-        for (Integer integer : numbers) {
-            if (integer != minVal) {
-                maxSum += integer;
+        int[] arr = new int[numbers.size()];
+        int index = 0;
+        for (Integer number : numbers
+        ) {
+            arr[index] = number;
+            index++;
+        }
+
+        int minElement = 0;
+        int maxElement = 0;
+        int sum = 0;
+
+        minElement = arr[0];
+        maxElement = minElement;
+        sum = minElement;
+
+        for (int i = 1; i < arr.length; i++) {
+            sum += arr[i];
+            if (arr[i] < minElement) {
+                minElement = arr[i];
+            }
+            if (arr[i] > maxElement) {
+                maxElement = arr[i];
             }
         }
 
-        return maxSum;
+        return sum - minElement;
     }
 
     @Override
     public int minSum(Collection<Integer> numbers) {
         validateParams(numbers);
-        int maxVal = Collections.max(numbers);
-        int minSum = 0;
 
-        for (Integer integer : numbers) {
-            if (integer != maxVal) {
-                minSum += integer;
+        int[] arr = new int[numbers.size()];
+        int index = 0;
+        for (Integer number : numbers
+        ) {
+            arr[index] = number;
+            index++;
+        }
+
+        int minElement = 0;
+        int maxElement = 0;
+        int sum = 0;
+
+        minElement = arr[0];
+        maxElement = minElement;
+        sum = minElement;
+
+        for (int i = 1; i < arr.length; i++) {
+            sum += arr[i];
+            if (arr[i] < minElement) {
+                minElement = arr[i];
+            }
+            if (arr[i] > maxElement) {
+                maxElement = arr[i];
             }
         }
 
-        return minSum;
+        return sum - maxElement;
     }
 
     /**
