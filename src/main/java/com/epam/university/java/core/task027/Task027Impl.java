d1b113a3f618994f56a7beed5a459d936d242f07
absintminded <absinthminded666@gmail.com>
2020-11-19 15:30:57 +0300

    new tests fixes

--- src/main/java/com/epam/university/java/core/task027/Task027Impl.java
+++ src/main/java/com/epam/university/java/core/task027/Task027Impl.java
@@ -9,57 +9,34 @@ public class Task027Impl implements Task027 {
     public Collection<Integer> extract(String sourceString) {
         if (sourceString == null) {
             throw new IllegalArgumentException();
-        } else if (sourceString.length() == 1) {
+        }
+
+        if (sourceString.charAt(0) == '0') {
             return new ArrayList<>();
         }
 
-        List<Integer> resultList = new ArrayList<>();
-        String source = sourceString;
+        List<Integer> numbers = new ArrayList<>();
 
-        int first = Character.getNumericValue(source.charAt(0));
-        int second = Character.getNumericValue(source.charAt(1));
-        int third = Character.getNumericValue(source.charAt(2));
+        for (int i = 1; i < sourceString.length() / 2; i++) {
 
+            String substring = sourceString.substring(0, i);
+            Integer number = Integer.parseInt(substring);
+            numbers.add(number);
 
-        if (first == 1 && second != 0) {
-            String[] newOne = source.split("");
-            for (String s : newOne) {
-                resultList.add(Integer.parseInt(s));
+            while (substring.length() < sourceString.length()) {
+                number++;
+                substring += number;
+                numbers.add(number);
+                if (substring.length() > sourceString.length()
+                        || !substring.equals(sourceString.substring(0, substring.length()))) {
+                    break;
+                }
             }
-        }
-
-        if (first == 1 && second == 0) {
-            String[] newOne = source.split("(?<=\\G.{2})");
-            for (String s : newOne) {
-                resultList.add(Integer.parseInt(s));
+            if (substring.equals(sourceString)) {
+                return numbers;
             }
+            numbers.clear();
         }
-
-        if (first == second && third != 9) {
-            resultList.add(Integer.parseInt(source.substring(0, 2)));
-            resultList.add(Integer.parseInt(source.substring(2,5)));
-            resultList.add(Integer.parseInt(source.substring(5)));
-        }
-
-        int fourth = Character.getNumericValue(source.charAt(3));
-
-        if (first == second && third == 9 && fourth != 8) {
-            resultList.add(Integer.parseInt(source.substring(0, 3)));
-            resultList.add(Integer.parseInt(source.substring(3,7)));
-            resultList.add(Integer.parseInt(source.substring(7)));
-        }
-
-        if (first == second && third == 9 && fourth == 8) {
-            if (Integer.parseInt(source.substring(8)) > Integer.parseInt(source.substring(4,8))) {
-                resultList.add(Integer.parseInt(source.substring(0,4)));
-                resultList.add(Integer.parseInt(source.substring(4,8)));
-                resultList.add(Integer.parseInt(source.substring(8)));
-            } else {
-                return new ArrayList<>();
-            }
-
-        }
-
-        return resultList;
+        return new ArrayList<>();
     }
 }
